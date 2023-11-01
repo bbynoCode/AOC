d235c32b7eed60f2985114f93f7c31d4e64833ea
bbynoCode <dlemm@csuchico.edu>
2023-10-31 18:40:32 -0700

    2022 Day 4 complete

---
+++ 2022/day4/day4b.go
@@ -0,0 +1,40 @@
+package main
+ 
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+	"strconv"
+)
+ 
+func main() {
+
+	var overlap int
+    filePath := "input.txt"
+    readFile, _ := os.Open(filePath)
+	defer readFile.Close()
+  
+    fileScanner := bufio.NewScanner(readFile)
+
+    for fileScanner.Scan(){
+		elves := strings.Split(fileScanner.Text(), ",")
+		// fmt.Println(elves)
+		elfOne := strings.Split(elves[0], "-")
+		elfTwo := strings.Split(elves[1], "-")
+
+		elfOneLow, _ := strconv.Atoi(elfOne[0])
+		elfOneHigh, _ := strconv.Atoi(elfOne[1])
+		elfTwoLow, _ := strconv.Atoi(elfTwo[0])
+		elfTwoHigh, _ := strconv.Atoi(elfTwo[1])
+		//elfOne is inside of elfTwo
+		if elfOneLow <= elfTwoHigh && elfOneHigh >= elfTwoLow {
+			overlap += 1
+
+		//elfTwo is inside of elfOne
+		} else if elfOneLow <= elfTwoHigh && elfOneHigh >= elfTwoLow {
+			overlap += 1
+		}
+	}
+	fmt.Printf("Number of elves that overlap: %d\n",overlap)
+}
