c3f6fb6e22ffedf3e937c4a718bb7d1b9608ec28
bbynoCode <dlemm@csuchico.edu>
2023-11-02 13:33:12 -0700

    2022 Day 6 complete

---
+++ 2022/day6/day6b.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+
+
+func main(){
+
+	input, _ := os.Open("input.txt")
+	defer input.Close()
+	scanner := bufio.NewScanner(input)
+
+	scanner.Scan()
+	s := scanner.Text()
+
+	const diffChars = 14
+
+	for i := range s {
+		chars := make(map[byte]bool)
+		for j:=0; j<diffChars; j++{
+			chars[s[i+j]]=true
+		}
+		if len(chars) == diffChars{
+			fmt.Println(i+diffChars)
+			break
+		}
+	}
+}
+
